60a5e905ff4ad9380acc69aa8427ca4b17e3be81
MarcusHellkvist <hellqvistmarcus@gmail.com>
2021-03-08 16:27:49 +0100

    added to many in all core data relationships so more than one user can add courses

--- CoreDataDemo/Models/Course+CoreDataProperties.swift
+++ CoreDataDemo/Models/Course+CoreDataProperties.swift
@@ -21,13 +21,64 @@ extension Course {
     @NSManaged public var id: Int64
     @NSManaged public var length: Int64
     @NSManaged public var price: Int64
+    @NSManaged public var progression: Float
     @NSManaged public var rating: Int64
     @NSManaged public var teacher: String?
     @NSManaged public var title: String?
-    @NSManaged public var progression: Float
-    @NSManaged public var ofUserBought: User?
-    @NSManaged public var ofUserEnrolled: User?
-    @NSManaged public var ofUserWishlist: User?
+    @NSManaged public var ofUserBought: NSSet?
+    @NSManaged public var ofUserEnrolled: NSSet?
+    @NSManaged public var ofUserWishlist: NSSet?
+
+}
+
+// MARK: Generated accessors for ofUserBought
+extension Course {
+
+    @objc(addOfUserBoughtObject:)
+    @NSManaged public func addToOfUserBought(_ value: User)
+
+    @objc(removeOfUserBoughtObject:)
+    @NSManaged public func removeFromOfUserBought(_ value: User)
+
+    @objc(addOfUserBought:)
+    @NSManaged public func addToOfUserBought(_ values: NSSet)
+
+    @objc(removeOfUserBought:)
+    @NSManaged public func removeFromOfUserBought(_ values: NSSet)
+
+}
+
+// MARK: Generated accessors for ofUserEnrolled
+extension Course {
+
+    @objc(addOfUserEnrolledObject:)
+    @NSManaged public func addToOfUserEnrolled(_ value: User)
+
+    @objc(removeOfUserEnrolledObject:)
+    @NSManaged public func removeFromOfUserEnrolled(_ value: User)
+
+    @objc(addOfUserEnrolled:)
+    @NSManaged public func addToOfUserEnrolled(_ values: NSSet)
+
+    @objc(removeOfUserEnrolled:)
+    @NSManaged public func removeFromOfUserEnrolled(_ values: NSSet)
+
+}
+
+// MARK: Generated accessors for ofUserWishlist
+extension Course {
+
+    @objc(addOfUserWishlistObject:)
+    @NSManaged public func addToOfUserWishlist(_ value: User)
+
+    @objc(removeOfUserWishlistObject:)
+    @NSManaged public func removeFromOfUserWishlist(_ value: User)
+
+    @objc(addOfUserWishlist:)
+    @NSManaged public func addToOfUserWishlist(_ values: NSSet)
+
+    @objc(removeOfUserWishlist:)
+    @NSManaged public func removeFromOfUserWishlist(_ values: NSSet)
 
 }
 
